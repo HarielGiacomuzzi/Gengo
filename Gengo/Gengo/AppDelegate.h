b8beff4317117475d590e9a0f099bec33fab5275
Daniel Amarante <danielamarante1@jabberwocky.local>
2015-03-20 13:27:22 -0300

    view controller skeleton added

---
+++ Gengo/Gengo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gengo
+//
+//  Created by Daniel Amarante on 3/20/15.
+//  Copyright (c) 2015 Daniel Amarante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gengo/Gengo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gengo
+//
+//  Created by Daniel Amarante on 3/20/15.
+//  Copyright (c) 2015 Daniel Amarante. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
