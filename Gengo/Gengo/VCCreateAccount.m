773789928b7548b50963ea0f314d58775adfed75
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-05-20 16:26:59 -0300

    added login funcs

--- Gengo/Gengo/VCCreateAccount.m
+++ Gengo/Gengo/VCCreateAccount.m
@@ -9,10 +9,16 @@
 #import "VCCreateAccount.h"
 
 @interface VCCreateAccount ()
+//@property (weak, nonatomic) IBOutlet UITextField *txt_nome;
+//@property (weak, nonatomic) IBOutlet UITextField *txt_email;
+//@property (weak, nonatomic) IBOutlet UITextField *txt_password;
+//@property (weak, nonatomic) IBOutlet UITextField *txt_confirm_password;
 @property (weak, nonatomic) IBOutlet UITextField *txt_nome;
 @property (weak, nonatomic) IBOutlet UITextField *txt_email;
 @property (weak, nonatomic) IBOutlet UITextField *txt_password;
 @property (weak, nonatomic) IBOutlet UITextField *txt_confirm_password;
+
+
 
 @end
 
@@ -32,13 +38,58 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(BOOL)ValidateInfo{
+    if (![self.txt_nome.text isEqualToString:@""] && self.txt_nome != nil && ![self.txt_nome.text isEqualToString:@" "]) {
+        if ([self.txt_password.text isEqualToString:self.txt_confirm_password.text] && self.txt_password!= nil) {
+            NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}";
+            NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+            bool a = [emailTest evaluateWithObject:self.txt_email.text];
+            if(a){
+                PFQuery *query = [PFQuery queryWithClassName:@"User"];
+                [query whereKey:@"email" equalTo:self.txt_email.text];
+                dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+                    NSArray *returnedItems = [query findObjects];
+                    if (returnedItems.count == 0) {
+                        PFUser *newUser = [[PFUser alloc] init];
+                        newUser.username = self.txt_email.text;
+                        newUser.password = self.txt_password.text;
+                        newUser[@"userReference"] = [User createNewUser:self.txt_nome.text email:self.txt_email.text andGender:@"male"];
+                        [newUser signUp];
+                        dispatch_async(dispatch_get_main_queue(), ^(void){
+                            [self dismissViewControllerAnimated:YES completion:nil];
+                        });
+                    }else{
+                        dispatch_async(dispatch_get_main_queue(), ^(void){
+                            UIAlertView *a = [[UIAlertView alloc]initWithTitle:@"Error" message:@"E-mail já cadastrado" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+                            [a show];
+                        });
+                    }
+                });
+                
+            //return a;
+            }else{
+                UIAlertView *a = [[UIAlertView alloc]initWithTitle:@"Error" message:@"E-mail inválido\nPor favor insira um e-mail válido" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+                [a show];
+                return NO;
+            }
+        }else{
+            UIAlertView *a = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Senha inválida\nPor favor insira uma senha válida" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [a show];
+            return NO;
+        }
+    }else{
+        UIAlertView *a = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Nome inválido\nPor favor insira um nome válido" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [a show];
+    }
+    return NO;
+}
+
 - (IBAction)CreateAccount:(id)sender {
-    PFUser *newUser = [[PFUser alloc] init];
-    newUser.username = self.txt_nome.text;
-    newUser.password = self.txt_password.text;
-    newUser[@"userReference"] = [User createNewUser:self.txt_nome.text email:self.txt_email.text andGender:@"male"];
-    [newUser signUp];
     
+    if ([self ValidateInfo]) {
+        
+    }
 }
 
 -(void)dismissKeyboard {
--- Gengo/Gengo/VCLogin.m
+++ Gengo/Gengo/VCLogin.m
@@ -11,6 +11,10 @@
 @interface VCLogin ()
 @property (weak, nonatomic) IBOutlet UITextField *txt_email;
 @property (weak, nonatomic) IBOutlet UITextField *txt_password;
+@property (weak, nonatomic) IBOutlet FBLoginView *fbLoginView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *loader;
+
+@property BOOL isLogged;
 
 @end
 
@@ -19,6 +23,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.fbLoginView initWithPermissions:@[@"public_profile",@"email"]];
+    self.fbLoginView.delegate = self;
     // Do any additional setup after loading the view.
 }
 
@@ -28,14 +34,48 @@
 }
 
 - (IBAction)btnLogin:(id)sender {
-    PFUser *user = [PFUser logInWithUsername:self.txt_email.text password:self.txt_password.text];
-    if (user != nil) {
-        [User lodUserWithObject:user[@"userReference"]];
-        if ([User loadUser] != nil) {
-            [self performSegueWithIdentifier:@"segueLoginTest" sender:self];
-        }
-    }
+    [self.loader startAnimating];
+    dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0), ^{
+        PFUser *user = [PFUser logInWithUsername:self.txt_email.text password:self.txt_password.text];
+        [user[@"userReference"] fetchIfNeeded];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (user != nil) {
+                [User lodUserWithObject:user[@"userReference"]];
+                if ([User loadUser] != nil) {
+                    [self.loader stopAnimating];
+                    [self performSegueWithIdentifier:@"loginSegue" sender:self];
+                }
+            }else{
+                UIAlertView *a = [[UIAlertView alloc]initWithTitle:@"Error" message:@"Login Inválido" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+                [a show];
+            }
+        });
+    });
     
+}
+
+- (IBAction)logginButton:(id)sender {
+    //FBLoginView *loginView = [[FBLoginView alloc] initWithPermissions:@[@"public_profile",@"email"]];
+    //loginView.delegate = self;
+}
+
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView
+                            user:(id<FBGraphUser>)user{
+    
+    if (FBSession.activeSession.state == FBSessionStateOpen && self.isLogged == NO) {
+        self.isLogged = YES;
+        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void){
+            [User lodUserWithEmail:[user objectForKey:@"email"] andUser:user];
+            dispatch_async(dispatch_get_main_queue(), ^(void){
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            });
+        });
+        
+    }
+}
+
+- (IBAction)unwindToLogin:(UIStoryboardSegue *)unwindSegue
+{
 }
 
 @end
